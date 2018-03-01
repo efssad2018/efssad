4b2a5a98786c7d23fecd3e7f029f18456d8512e9
youping <ongyouping@outlook.com>
2018-03-01 14:11:28 +0800

    project folder - main and site Commander

---
+++ mainCommander/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MaincommanderConfig(AppConfig):
+    name = 'mainCommander'
---
+++ siteCommander/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SitecommanderConfig(AppConfig):
+    name = 'siteCommander'
