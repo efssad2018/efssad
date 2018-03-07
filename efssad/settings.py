b8bbc9cfa792abe2cb2fbfb0249606bdaf7abe2d
youping <ongyouping@outlook.com>
2018-03-07 18:10:35 +0800

    update on DB

--- efssad/settings.py
+++ efssad/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'efssad_back.apps.CommanderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
