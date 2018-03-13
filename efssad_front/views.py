303e8027f77bc5285060c34de7deab37025bcf02
Heryani <heryanimacaroni@gmail.com>
2018-03-14 02:52:50 +0800

    map added

--- efssad_front/views.py
+++ efssad_front/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
-# from efssad_back.models import Mission
+from efssad_back.models import Mission, Account
 
 # Create your views here.
 def login(request):
