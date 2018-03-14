9594c283a8adcdaaf056cc9570ef9644399380bb
Fiqah Sapnan <iqah_nurul@hotmail.com>
2018-03-14 22:33:15 +0800

    updated

--- efssad_back/views.py
+++ efssad_back/views.py
@@ -4,14 +4,14 @@ from django.template import loader
 from efssad_back.models import Mission, Account
 
 # Create your views here.
-#def login(request):
+# def login(request):
 
     # all_mission = Mission.objects.all()
     # template = loader.get_template('index/login.html')
     # return HttpResponse(template.render(context, request))
 
     # return HttpResponse('<h1>test</h1>')
-    #return render(request,'registration/login.html')
+    # return render(request,'registration/login.html')
     # return render(request, 'efssad_front/MCmain.html', context)
     # return render(request, 'efssad_front/MCarchive.html', context)
     # return render(request, 'efssad_front/MCmission.html', context)
@@ -19,7 +19,8 @@ from efssad_back.models import Mission, Account
     # return render(request, 'efssad_front/SCmission.html', context)
 
 def mcmain(request):
-    return render(request, 'efssad_front/MCmain.html')
+    # return render(request, 'efssad_front/MCmain.html')
+    return render(request, 'efssad_front/MCmission.html')
 
 def scmission(request):
     return render(request, 'efssad_front/SCmission.html')
@@ -29,4 +30,5 @@ def mission(request):
 
 def archive(request):
     return render(request, 'efssad_front/MCarchive.html')
+
 
