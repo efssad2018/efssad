f3a6e74568d4229266484c8323842be19c489531
Heryani <heryanimacaroni@gmail.com>
2018-03-14 13:26:54 +0800

    merge front and back

--- efssad_back/urls.py
+++ efssad_back/urls.py
@@ -1,7 +1,7 @@
 from django.urls import re_path
 from . import views
 
-app_name = "efssad_front"
+app_name = "efssad_back"
 
 urlpatterns = [
     #re_path(r'^$/', views.login, name='login'),
--- efssad_front/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EfssadFrontConfig(AppConfig):
-    name = 'efssad_front'
